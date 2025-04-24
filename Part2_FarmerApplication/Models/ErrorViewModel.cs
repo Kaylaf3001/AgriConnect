b767a3d52f88bd2e088cafd8a77540ab8cd5e0d1
Kayla <Kaylaf3001@gmail.com>
2025-04-24 14:24:28 +0200

    Add project files.

---
+++ Part2_FarmerApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Part2_FarmerApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
